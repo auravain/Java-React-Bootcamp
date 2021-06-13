44bf008551bfa0867a32ad3ca5d7ab327ee965d2
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-06-13 16:20:20 +0300

    added UserService

---
+++ 0.Homeworks/Day9/hw1/hrms/hrms/src/main/java/kodlamaio/hrms/business/abstracts/UserService.java
@@ -0,0 +1,14 @@
+package kodlamaio.hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.hrms.entities.concretes.User;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+
+public interface UserService {
+	DataResult<List<User>> getAll();
+	Result add(User user);
+	DataResult<User> getById(int id);
+	
+}
