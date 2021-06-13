0898d8ccd4be4bc30319afca038fb102fbed657c
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-06-13 15:09:55 +0300

    added skills feature

---
+++ 0.Homeworks/Day9/hw1/hrms/hrms/src/main/java/kodlamaio/hrms/entities/concretes/Skills.java
@@ -0,0 +1,35 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "skills")
+public class Skills {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "technology_name")
+	private String techName;
+	
+	@ManyToOne
+    @JoinColumn(name = "candidates_id")
+    private Candidates candidates;
+
+}
